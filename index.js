0a57b2e9c6f4d28ed76e6ab1179cb144b0af25ab
Michael Gregson <mgregson@beam.io>
2019-03-01 14:19:48 -0800

    implement the beam function argument style rule in solhint

---
+++ index.js
@@ -0,0 +1,8 @@
+/* -*- c-basic-offset: 2 -*- */
+/* eslint-env node */
+/* global module require */
+const UnderscoreFunctionArgs = require('./src/UnderscoreFunctionArgs');
+
+module.exports = [
+  UnderscoreFunctionArgs,
+];
